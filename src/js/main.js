97d18975089a1b48af115683e8a2712437398000
FatimaDuret <f.duret.g@gmail.com>
2024-04-29 14:05:35 +0200

    cambios js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 const btnSearch = document.querySelector('.js_btn-search');
@@ -13,110 +14,162 @@ let favDrinks = [];
 
 
 const renderOneDrink = (eachDrink) => {
-    let html= "";
-    const indexFav = favDrinks.findIndex((item) => item.id === eachDrink.id
-  );
-
-  let classCss = indexFav === -1 ? '' : 'fav';
-
-        html = `<li class="js_drinks" id="${eachDrink.id}">
-        <h4>${eachDrink.name}</h4>
-        <div class="js_drink-img" style="background-image: url(${eachDrink.image});"></div>`;
-    
-        for ( const iterator of eachDrink.name) {
-            html += `<img src="${eachDrink.image}" alt="${eachDrink.name}" class="js_drink-img">`;
-        }
-        html += `</li>`;
-
-        return html;
-};
+    let html = "";
+    const indexFav = favDrinks.findIndex((item) => item.id === eachDrink.idDrink);
+    let classCss = indexFav === -1 ? '' : 'fav';
+  
+    html = `<li class="js_drinks ${classCss}" id="${eachDrink.idDrink}">
+              <h4>${eachDrink.strDrink}</h4>
+              <img src="${eachDrink.strDrinkThumb}" alt="${eachDrink.strDrink}" class="js_drink-img">
+            </li>`;
+  
+    return html;
+  };
 
 
 const addFavorite = (ev) => {
+    const liClickedId = ev.currentTarget.id;
+    const clickedDrinkData = cocktailsData.find((item) => item.idDrink === liClickedId);
+  
+    const favoriteLiClicked = favDrinks.findIndex((item) => item.idDrink === liClickedId);
+  
+    if (favoriteLiClicked === -1) {
+        favDrinks.push(clickedDrinkData);
+        ev.currentTarget.classList.add('fav'); 
+        const favListItem = document.createElement('li');
+        favListItem.classList.add('js_drinks', 'fav');
+        favListItem.id = liClickedId;
+        favListItem.innerHTML = `<h4>${clickedDrinkData.strDrink}</h4>
+                                 <img src="${clickedDrinkData.strDrinkThumb}" alt="${clickedDrinkData.strDrink}" class="js_drink-img">`;
+        ulListFav.appendChild(favListItem); 
+    } else {
+        favDrinks.splice(favoriteLiClicked, 1);
+        ev.currentTarget.classList.remove('fav'); 
+        const favListItemToRemove = document.getElementById(liClickedId);
+        favListItemToRemove.remove(); 
+    }
 
-  const liClickedId = ev.currentTarget.id;
-  const clickedDrinkData = cocktailsData.find((item) => item.id === liClickedId
-
-  );
-
-  const favoriteLiClicked = favDrinks.findIndex((item) => item.id === liClickedId
-
-  );
-
-  if (favoriteLiClicked === -1) {
-    favDrinks.push(clickedDrinkData);
-  } else{
-    favDrinks.splice(favoriteLiClicked, 1);
-   }
-
-   renderAllDrinks(cocktailsData);
-   localStorage.setItem('favDrinks', JSON.stringify(favDrinks));
-
- 
     
+    renderAllDrinks(cocktailsData);
+  
+    localStorage.setItem('favDrinks', JSON.stringify(favDrinks));
 };
+
+
 
 const renderAllDrinks = (array) => {
-    ulListResults.innerHTML= '';
-    for (let i=0; i<array.length; i++) {
-        ulListResults.innerHTML += renderOneDrink(array[i]);
+    let drinksHTML = ''; 
+    for (let i = 0; i < array.length; i++) {
+        const isFav = favDrinks.find((favDrink) => favDrink.idDrink === array[i].idDrink) !== undefined;
+        const classCss = isFav ? 'fav' : ''; 
+        
+        drinksHTML += `<li class="js_drinks ${classCss}" id="${array[i].idDrink}">
+                            <h4>${array[i].strDrink}</h4>
+                            <img src="${array[i].strDrinkThumb}" alt="${array[i].strDrink}" class="js_drink-img">
+                        </li>`; 
     }
+    ulListResults.innerHTML = drinksHTML; 
 
     const allDrinksLi = document.querySelectorAll('.js_drinks');
-    for (const li of allDrinksLi) {
-        li.addEventListener('click', addFavorite);
-    }
-
+    allDrinksLi.forEach((li) => {
+        li.addEventListener('click', (event) => {
+            addFavorite(event);
+            if (!li.classList.contains('fav')) {
+                li.classList.add('fav'); 
+            }
+        });
+    });
 };
 
 
 
+const renderFavDrinks = (favDrinks) => {
+    let favDrinksHTML = '';
+    for (const drink of favDrinks) {
+        favDrinksHTML += renderOneDrink(drink); 
+    }
+    ulListFav.innerHTML = favDrinksHTML; 
+};
 
-const getData = () => {
+
+  const getData = (searchTerm) => {
     fetch(
-      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
     )
       .then((response) => response.json())
       .then((data) => {
         cocktailsData = data.drinks;
         renderAllDrinks(cocktailsData);
         localStorage.setItem('drinks', JSON.stringify(cocktailsData));
-        
       });
-  };
+};
 
-  const handleSearch = () => {
-    const valueSearch = inputSearch.value;
-    const filteredDrinks = cocktailsData.filter((item) =>
-      item.name.toLowerCase().includes(valueSearch.toLowerCase())
+ 
+
+
+const handleSearch = (event) => {
+    event.preventDefault();
+    const valueSearch = inputSearch.value.toLowerCase().trim(); 
     
-    );
-    renderAllDrinks(filteredDrinks);
-
-   
-  };
-
-  const init = () => {
-   
-    const drinksFavLocal = localStorage.getItem('favDrinks');
-     if (drinksFavLocal !== null) {
-     favDrinks = JSON.parse(drinksFavLocal);
-     renderAllDrinks(cocktailsData);
-     }
-
-     const drinksLocal = localStorage.getItem('drinks');
-     if (drinksLocal !== null) {
-     cocktailsData = JSON.parse(drinksLocal);
-     renderAllDrinks(cocktailsData);
-    } else {
-     getData();
-  
+    let filteredDrinks = [];
+    
+    
+    if (favDrinks.length > 0) {
+        filteredDrinks = favDrinks.filter((item) => item.strDrink.toLowerCase().includes(valueSearch));
     }
-  };
+    
+    
+    if (cocktailsData.length > 0) {
+        const drinksFromAPI = cocktailsData.filter((item) => item.strDrink.toLowerCase().includes(valueSearch));
+        filteredDrinks = filteredDrinks.concat(drinksFromAPI);
+    }
 
+   
+    if (filteredDrinks.length === 0) {
+        getData(valueSearch);
+        return; 
+    }
+    
+    
+    renderAllDrinks(filteredDrinks);
+};
+
+ 
+
+const init = () => {
+    const drinksFavLocal = localStorage.getItem('favDrinks');
+    if (drinksFavLocal !== null) {
+        favDrinks = JSON.parse(drinksFavLocal);
+        renderFavDrinks(favDrinks); 
+    }
+
+    const drinksLocal = localStorage.getItem('drinks');
+    if (drinksLocal !== null) {
+        cocktailsData = JSON.parse(drinksLocal);
+        renderAllDrinks(cocktailsData);
+    } else {
+        getData();
+    }
+
+    
+    btnreset.addEventListener('click', () => {
+        
+        ulListFav.innerHTML = '';
+        
+        
+        favDrinks = [];
+        
+       
+        localStorage.removeItem('favDrinks');
+    });
+};
 
 init();
-inputSearch.addEventListener("input", handleSearch);
+
+
+btnSearch.addEventListener('click', handleSearch);
+
+
 
 
 
